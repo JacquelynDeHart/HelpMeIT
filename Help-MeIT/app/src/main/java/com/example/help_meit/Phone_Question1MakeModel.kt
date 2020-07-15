48d237810c2c85cead4a4739944aea04d79329cf
ambrosaur <49253746+ambrosaur@users.noreply.github.com>
2020-07-15 00:20:26 -0500

    update
    creating margins for phone_q xml

--- Help-MeIT/app/src/main/java/com/example/help_meit/Phone_Question1MakeModel.kt
+++ Help-MeIT/app/src/main/java/com/example/help_meit/Phone_Question1MakeModel.kt
@@ -3,6 +3,7 @@ package com.example.help_meit
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+
 class Phone_Question1MakeModel : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
