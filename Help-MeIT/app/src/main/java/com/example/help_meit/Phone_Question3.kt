a0fc63af80b9f3023028c4c9f5bcf01f249f3dd1
Jacquelyn <jacquelyn0890@gmail.com>
2020-07-17 09:38:04 -0500

    updateto phone question layouts

--- Help-MeIT/app/src/main/java/com/example/help_meit/Phone_Question3.kt
+++ Help-MeIT/app/src/main/java/com/example/help_meit/Phone_Question3.kt
@@ -1,11 +1,32 @@
 package com.example.help_meit
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import kotlinx.android.synthetic.main.activity_phone__question3.*
 
 class Phone_Question3 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_phone__question3)
+
+        val btn_yesq3 = findViewById<View>(R.id.btn_yesq3) as Button
+        btn_yesq3.setOnClickListener {
+            btn_nextq3.setVisibility(View.VISIBLE)
+        }
+
+        val btn_noq3 = findViewById<View>(R.id.btn_noq3) as Button
+        btn_noq3.setOnClickListener {
+            txt_phoneq3.setVisibility(View.VISIBLE)
+            btn_nextq3.setVisibility(View.INVISIBLE)
+        }
+
+        val btn_nextq3 = findViewById<View>(R.id.btn_nextq3) as Button
+        btn_nextq3.setOnClickListener {
+            val intent = Intent(this, Phone_Question4::class.java)
+            startActivity(intent)
+        }
     }
 }
--- Help-MeIT/app/src/main/java/com/example/help_meit/Phone_Question4.kt
+++ Help-MeIT/app/src/main/java/com/example/help_meit/Phone_Question4.kt
@@ -1,11 +1,32 @@
 package com.example.help_meit
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import kotlinx.android.synthetic.main.activity_phone__question4.*
 
 class Phone_Question4 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_phone__question4)
+
+        val btn_yesq4 = findViewById<View>(R.id.btn_yes) as Button
+        btn_yes.setOnClickListener {
+            btn_next.setVisibility(View.VISIBLE)
+        }
+
+        val btn_noq4 = findViewById<View>(R.id.btn_no) as Button
+        btn_no.setOnClickListener {
+            text_phoneq4.setVisibility(View.VISIBLE)
+            btn_next.setVisibility(View.INVISIBLE)
+        }
+
+        val btn_nextq4 = findViewById<View>(R.id.btn_next) as Button
+        btn_next.setOnClickListener {
+            val intent = Intent(this, Phone_Question5::class.java)
+            startActivity(intent)
+        }
     }
 }
