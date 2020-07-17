1845fcfed4adc9ccba566e9f6ef8729f27c9570c
Jacquelyn <jacquelyn0890@gmail.com>
2020-07-16 21:30:23 -0500

    added stuff to Phone_Question1MakeModel.kt

--- Help-MeIT/app/src/main/java/com/example/help_meit/Phone_Question1MakeModel.kt
+++ Help-MeIT/app/src/main/java/com/example/help_meit/Phone_Question1MakeModel.kt
@@ -1,12 +1,23 @@
 package com.example.help_meit
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 
 class Phone_Question1MakeModel : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_phone__question1_make_model)
+
+        val btn_commit = findViewById<View>(R.id.btn_commit) as Button
+        btn_commit.setOnClickListener {
+            val intent = Intent(this, Phone_Question2::class.java)
+            startActivity(intent)
+        }
+
     }
+
 }
