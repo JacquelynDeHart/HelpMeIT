7ef02c727e0a2f36036d3563dfd8d73ed53dcbc0
ambrosaur <49253746+ambrosaur@users.noreply.github.com>
2020-07-20 18:09:36 -0500

    console_q5 and add q6 file
    added console_q6 files and questions for q5

---
+++ Help-MeIT/app/src/main/java/com/example/help_meit/Console_Question6.kt
@@ -0,0 +1,15 @@
+package com.example.help_meit
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Console_Question6 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_console__question6)
+    }
+    override fun finish() {
+        super.finish()
+        overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right)
+    }
+}
