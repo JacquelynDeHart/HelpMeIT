4b33431415bdaa4f0d9d89651cc4ae65c3a0f89d
ambrosaur <49253746+ambrosaur@users.noreply.github.com>
2020-07-20 17:43:34 -0500

    update
    console q2 and q3

--- Help-MeIT/app/src/main/java/com/example/help_meit/Console_Question2.kt
+++ Help-MeIT/app/src/main/java/com/example/help_meit/Console_Question2.kt
@@ -5,29 +5,36 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import kotlinx.android.synthetic.main.activity_console__question2.*
+import kotlinx.android.synthetic.main.activity_p_c__question2.*
 import kotlinx.android.synthetic.main.activity_phone__question2.*
+import kotlinx.android.synthetic.main.activity_phone__question2.btn_next
+import kotlinx.android.synthetic.main.activity_phone__question2.btn_yes
+import kotlinx.android.synthetic.main.activity_phone__question2.textView_reply_no
 
 class Console_Question2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_console__question2)
 
-        val btn_yes = findViewById<View>(R.id.btn_yes) as Button
-        btn_yes.setOnClickListener {
-            textView_reply_no.setVisibility(View.INVISIBLE)
+        btn_consoleQ2_1.setOnClickListener {
+            txt_consoleA1.setVisibility(View.VISIBLE)
+            txt_consoleA2.setVisibility(View.INVISIBLE)
             btn_next.setVisibility(View.VISIBLE)
         }
 
-        val btn_nxt = findViewById<View>(R.id.btn_next) as Button
-        btn_nxt.setOnClickListener {
+        btn_consoleQ2_2.setOnClickListener {
+            txt_consoleA2.setVisibility(View.VISIBLE)
+            txt_consoleA1.setVisibility(View.INVISIBLE)
+            btn_next.setVisibility(View.VISIBLE)
+        }
+
+        btn_next.setOnClickListener {
             val intent = Intent(this, Console_Question3::class.java)
             startActivity(intent)
+            overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left)
         }
-        val btn_no = findViewById<View>(R.id.btn_no) as Button
-        btn_no.setOnClickListener {
-            textView_reply_no.setVisibility(View.VISIBLE)
-            btn_nxt.setVisibility(View.INVISIBLE)
-        }
+
     }
     override fun finish() {
         super.finish()
--- Help-MeIT/app/src/main/java/com/example/help_meit/Console_Question3.kt
+++ Help-MeIT/app/src/main/java/com/example/help_meit/Console_Question3.kt
@@ -1,12 +1,40 @@
 package com.example.help_meit
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import kotlinx.android.synthetic.main.activity_console__question2.*
+import kotlinx.android.synthetic.main.activity_console__question2.btn_next
+import kotlinx.android.synthetic.main.activity_console__question3.*
+import kotlinx.android.synthetic.main.activity_phone__question2.*
 
 class Console_Question3 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_console__question3)
+
+        val btn_consoleQ3_1: Button = findViewById<Button>(R.id.btn_consoleQ3_1)
+        val btn_consoleQ3_2: Button = findViewById<Button>(R.id.btn_consoleQ3_2)
+
+        btn_consoleQ3_1.setOnClickListener {
+            txt_consoleQ3A1.setVisibility(View.VISIBLE)
+            txt_consoleQ3A2.setVisibility(View.INVISIBLE)
+            btn_next.setVisibility(View.VISIBLE)
+        }
+
+        btn_consoleQ3_2.setOnClickListener {
+            txt_consoleQ3A2.setVisibility(View.VISIBLE)
+            txt_consoleQ3A1.setVisibility(View.INVISIBLE)
+            btn_next.setVisibility(View.VISIBLE)
+        }
+
+        btn_next.setOnClickListener {
+            val intent = Intent(this, Console_Question4::class.java)
+            startActivity(intent)
+            overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left)
+        }
     }
     override fun finish() {
         super.finish()
