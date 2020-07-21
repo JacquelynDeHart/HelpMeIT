61ddabf0ba09ea7e5ad3589a4eb67266581288cd
ambrosaur <49253746+ambrosaur@users.noreply.github.com>
2020-07-20 19:36:52 -0500

    console_q7 update
    questions for q7 added and edited

--- Help-MeIT/app/src/main/java/com/example/help_meit/Console_Question7.kt
+++ Help-MeIT/app/src/main/java/com/example/help_meit/Console_Question7.kt
@@ -1,12 +1,20 @@
 package com.example.help_meit
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_console__question2.*
+import kotlinx.android.synthetic.main.activity_console__question6.*
+import kotlinx.android.synthetic.main.activity_console__question6.btn_menuReturn
+import kotlinx.android.synthetic.main.activity_console__question6.btn_next1
+import kotlinx.android.synthetic.main.activity_console__question7.*
 
 class Console_Question7 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_console__question7)
+
     }
     override fun finish() {
         super.finish()
