b39d6a062d595ca5a15b17f882a985e374e6074a
ambrosaur <49253746+ambrosaur@users.noreply.github.com>
2020-07-20 20:01:03 -0500

    update console_q8.xml
    textview and buttons added in console_q8.xml

--- Help-MeIT/app/src/main/java/com/example/help_meit/Console_Question8.kt
+++ Help-MeIT/app/src/main/java/com/example/help_meit/Console_Question8.kt
@@ -1,4 +1,20 @@
 package com.example.help_meit
 
-class Console_Question8 {
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_console__question8.*
+
+class Console_Question8 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_console__question8)
+
+
+    }
+    override fun finish() {
+        super.finish()
+        overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right)
+    }
 }
